509d151c3e5fc1b37a743420050438153ec37f41
wuxudongxd <wuxudongxd@gmail.com>
2021-07-01 10:57:29 +0800

    feat: 增加样式

--- src/views/file/index.vue
+++ src/views/file/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-wrap px-20 pt-20">
+    <div class="flex flex-wrap px-10 pt-10">
         <span v-for="item in 12">
-            <span class="flex flex-col items-center">
+            <span class="flex flex-col items-center hover:bg-blue-100 rounded-md transition-all duration-200 ease-in-out">
                 <SvgIcon name="PDF" class="h-20 w-20 mx-4 my-6"></SvgIcon>
                 PDF文件
             </span>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="flex flex-row h-screen">
         <div class="w-52 bg-gray-100 shadow flex flex-col justify-between">
-            <div class="flex flex-col items-center space-y-7">
-                <span>
+            <div class="flex flex-col items-center">
+                <div class="h-28 flex flex-col">
                     <SvgIcon name="云盘" class="h-16 w-16 ml-3"></SvgIcon>简存取云盘
-                </span>
-                <a v-for="item in choiceItem" :key="item.id" @click="changeItem">
+                </div>
+                <div v-for="item in choiceItem" :key="item.id" @click="changeItem" class="h-12 w-full pl-10 flex items-center hover:bg-gray-300 transition-all duration-200 ease-in-out rounded-md cursor-pointer">
+                    <SvgIcon :name="item.icon" class="h-5 w-5 mr-3 mt-0.5"></SvgIcon>
                     {{
                         item.name
                     }}
-                </a>
+                </div>
             </div>
             <div>容量</div>
         </div>
@@ -25,13 +26,13 @@ import Headbar from "@/components/headbar.vue";
 import SvgIcon from '@/components/SvgIcon.vue';
 
 const choiceItem = [
-    { id: 1, name: "文件", link: "" },
-    { id: 2, name: "最近", link: "recently" },
-    { id: 3, name: "收藏", link: "collection" },
-    { id: 4, name: "分享", link: "share" },
-    { id: 5, name: "音乐", link: "music" },
-    { id: 6, name: "视频", link: "video" },
-    { id: 7, name: "图片", link: "picture" },
+    { id: 1, name: "文件", link: "", icon:"文件夹" },
+    { id: 2, name: "最近", link: "recently", icon:"时钟"},
+    { id: 3, name: "收藏", link: "collection", icon:"收藏" },
+    { id: 4, name: "分享", link: "share", icon:"分享" },
+    { id: 5, name: "音乐", link: "music", icon:"音乐" },
+    { id: 6, name: "视频", link: "video", icon:"视频" },
+    { id: 7, name: "图片", link: "picture", icon:"图片" },
 ];
 const changeItem = (param: any) => {
     for (const item of choiceItem) {
