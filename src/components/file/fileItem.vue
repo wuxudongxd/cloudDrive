37564cd141e1296e918dfadcf957a524a0b927d1
wuxudongxd <wuxudongxd@gmail.com>
2021-07-05 19:49:46 +0800

    feat: 封装了文件组件

--- src/components/file/fileItem.vue
+++ src/components/file/fileItem.vue
@@ -1,9 +1,9 @@
 <template>
   <span
-    class="flex flex-col items-center hover:bg-blue-100 rounded-md transition-all duration-200 ease-in-out"
+    class="flex flex-col items-center px-4 mx-2 py-4 my-2 hover:bg-blue-100 rounded-md transition duration-200 ease-in-out"
   >
-    <SvgIcon :name="filetype" class="h-20 w-20 mx-4 my-6"></SvgIcon>
-    {{ filename }}
+    <SvgIcon :name="filetype" class="h-20 w-20 mb-3"></SvgIcon>
+    <div class="text-gray-700 text-sm w-24 h-5 text-center overflow-hidden">{{ filename }}</div>
   </span>
 </template>
 <script lang="ts" setup>
--- src/components/file/showFiles.vue
+++ src/components/file/showFiles.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="flex flex-wrap px-8 pt-5 pb-10">
-    <span>
-      <fileItem filetype="PDF" filename="PDF文件" />
-    </span>
+  <div class="flex flex-wrap content-start px-8 pt-5 pb-10">
+    <fileItem filetype="pdf" filename="PDF文件" />
+    <fileItem filetype="docx" filename="WORD文件" />
+    <fileItem filetype="folder" filename="文件夹" />
+    <fileItem filetype="image" filename="图片" />
+    <fileItem filetype="music" filename="音乐" />
+    <fileItem filetype="pptx" filename="PPT文件" />
+    <fileItem filetype="txt" filename="文本文件" />
+    <fileItem filetype="unknown" filename="未知文件" />
+    <fileItem filetype="video" filename="视频文件" />
+    <fileItem filetype="xlsx" filename="表格文件" />
+    <fileItem filetype="zip" filename="压缩文件1234" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import fileItem from "@/components/file/fileItem.vue";
-import router from "@/router";
+// import router from "@/router";
 // console.log(router.currentRoute.value.path.slice(1));
 </script>
